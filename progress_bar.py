d120e092c2e6422e63500666947aea43891908c2
Victor Pantoja <victor.pantoja@gmail.com>
2018-09-02 20:15:59 +0200

    Make it work properly with every number

--- progress_bar.py
+++ progress_bar.py
@@ -1,15 +1,19 @@
 import sys
 import time
+import math
 
-index = 0
-for url_dict in range(100):
-    time.sleep(0.1)
+n_messages = 650
+
+for index, url_dict in enumerate(range(n_messages)):
     index += 1
+    time.sleep(0.01)
+    
+    progress_index = math.floor(index/n_messages*100)
 
-    percentual = "%.2f%%" % index
-    sys.stdout.write('\r[{0}{1}] {2}'.format('#' * index,
-                                             ' ' * (100-index),
-                                           percentual))
+    percentual = "%.2f%%" % progress_index
+    sys.stdout.write('\r[{0}{1}] {2}'.format('#' * progress_index,
+                                              ' ' * (100-progress_index),
+                                            percentual))
     sys.stdout.flush()
     
 print("")
